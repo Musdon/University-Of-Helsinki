79be59c7548315817370deb521e7ea592a31aaa0
Musdon <majibade5@gmail.com>
2024-11-02 11:07:00 +0100

    completed dividing individual elements into components

--- part1.6-1.14/src/App.js
+++ part1.6-1.14/src/App.js
@@ -17,17 +17,66 @@ function App() {
     setBad(bad + 1);
   }
 
+  function total(good, neutral, bad) {
+    return good + neutral + bad;
+  }
+  function percentage(good, neutral, bad) {
+    const totalVotes = total(good, neutral, bad);
+    return totalVotes === 0 ? 0 : (good / totalVotes) * 100;
+  }
+
+  const hasFeedback = total(good, neutral, bad) > 0;
+
+  function Statistics(props) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <StatisticsLine text="good" value={props.good} />
+        <StatisticsLine text="neutral" value={props.neutral} />
+        <StatisticsLine text="bad" value={props.bad} />
+        <StatisticsLine
+          text="all"
+          value={total(props.good, props.neutral, props.bad)}
+        />
+        <StatisticsLine
+          text={"Percentage"}
+          value={
+            percentage(props.good, props.neutral, props.bad).toFixed(2) + "%"
+          }
+        />
+      </div>
+    );
+  }
+
+  function Button({ onClick, text }) {
+    return <button onClick={onClick}>{text}</button>;
+  }
+
+  function StatisticsLine({ text, value }) {
+    return (
+      <p>
+        {text}:{value}
+      </p>
+    );
+  }
+
   return (
     <div>
       <h1> give feedback</h1>
-      <button onClick={increaseGood}>good</button>
-      <button onClick={increaseNeutral}>neutral</button>
-      <button onClick={increaseBad}>bad</button>
-
-      <h1>statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
+      <Button onClick={increaseGood} text={"good"} />
+      <Button onClick={increaseNeutral} text={"neutral"} />
+      <Button onClick={increaseBad} text={"bad"} />
+      {hasFeedback ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total()}
+          percentage={percentage()}
+        />
+      ) : (
+        <p>No feedback has been given yet</p>
+      )}
     </div>
   );
 }
