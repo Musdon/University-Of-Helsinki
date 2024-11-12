fbb56c0c20dd958c975b8de75d04d62061b4f393
Musdon <majibade5@gmail.com>
2024-11-12 14:39:52 +0100

    completed 2.2 and 2.3

--- part2/part2exercises/src/component/Course.js
+++ part2/part2exercises/src/component/Course.js
@@ -1,10 +1,15 @@
-import Header from "./Header";
-import Content from "./Content";
+import Header from "./Header.js";
+import Content from "./Content.js";
 const Course = ({ course }) => {
+  const totalExercises = course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
   return (
     <div>
       <Header courseName={course.name} />
       <Content parts={course.parts} />
+      <p>Total Exercises: {totalExercises}</p>
     </div>
   );
 };
