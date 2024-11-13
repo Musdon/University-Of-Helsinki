5d25800828690382d27605616c021c7862f125f6
Musdon <majibade5@gmail.com>
2024-11-13 16:44:11 +0100

    added phone number field to persons object

--- part2/exercise2.6-2.10/src/App.js
+++ part2/exercise2.6-2.10/src/App.js
@@ -3,9 +3,14 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const addPerson = (event) => {
@@ -14,9 +19,11 @@ const App = () => {
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already in the phone book`);
     } else {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, phoneNumber: newNumber }));
+
+      setNewName("");
+      setNewNumber("");
     }
-    setNewName("");
   };
 
   return (
@@ -27,13 +34,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+          <li key={index}>
+            {person.name} {person.phoneNumber}
+          </li>
         ))}
       </ul>
       ...
