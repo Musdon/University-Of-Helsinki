5a35b1998c8676bc76b32bbb52115013f0c964bd
Musdon <majibade5@gmail.com>
2024-11-15 12:40:49 +0100

    completed 2.11 rendered phonebook from json server

--- part2/exercise2.6-2.10/src/App.js
+++ part2/exercise2.6-2.10/src/App.js
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
+  const hook = () => {
+    console.log("entering the axios");
+    axios.get("http://localhost:3002/persons").then((response) => {
+      console.log(response.data);
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
+  console.log("render", persons.length, "persons");
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
@@ -13,22 +24,34 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchField = (event) => {
+    setSearch(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
 
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already in the phone book`);
     } else {
-      setPersons(persons.concat({ name: newName, phoneNumber: newNumber }));
-
+      setPersons(persons.concat({ name: newName, number: newNumber }));
       setNewName("");
       setNewNumber("");
     }
   };
 
+  const personsToShow = search
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(search.toLowerCase())
+      )
+    : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={search} onChange={handleSearchField} />
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -42,13 +65,12 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
+        {personsToShow.map((person, index) => (
           <li key={index}>
-            {person.name} {person.phoneNumber}
+            {person.name} {person.number}
           </li>
         ))}
       </ul>
-      ...
     </div>
   );
 };
